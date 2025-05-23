a435f9b81305917558e7e45625a17437d519bac0
PaulKelvin <Ibukunadesanya0@gmail.com>
2025-05-23 17:25:54 +0100

    Fix reviews page background color to maintain consistent grey shade after header

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -2,7 +2,7 @@ import ReviewSection from '@/components/home/ReviewSection';
 
 export default function ReviewsPage() {
   return (
-    <div className="pt-32">
+    <div className="pt-32 bg-[#F8F9FA]">
       <ReviewSection />
     </div>
   );
