a742a9f178556f125482891e8d0d2cef37fd1132
PaulKelvin <Ibukunadesanya0@gmail.com>
2025-05-23 17:34:05 +0100

    Further reduce spacing after header in reviews page from pt-24 to pt-18

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -2,7 +2,7 @@ import ReviewSection from '@/components/home/ReviewSection';
 
 export default function ReviewsPage() {
   return (
-    <div className="pt-24 bg-[#F8F9FA]">
+    <div className="pt-18 bg-[#F8F9FA]">
       <ReviewSection />
     </div>
   );
